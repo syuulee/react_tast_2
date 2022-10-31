267a04f69a70a4d0342a765fe88a175a43e93241
syuulee <yuyu00lee@gmail.com>
2022-10-31 16:40:52 +0900

    2022.10.31

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,97 +5,95 @@ const GNB = [
         id: 1,
         menu: '의료진/진료과',
         link: '',
-        smenu: {
-            stitle: '의료진 / 진료과',
-            scontent: [
-                '의료진',
-                '진료과',
-                '암병원',
-                '어린이병원',
-                '심장병원',
-                '센터',
-                '클리닉',
-                '아산생명과학연구원',
-                '기타',
-            ],
-        },
+        smenu:
+            [
+                { con: '의료진', link: "" },
+                { con: '진료과', link: "" },
+                { con: '암병원', link: "" },
+                { con: '어린이병원', link: "" },
+                { con: '심장병원', link: "" },
+                { con: '센터', link: "" },
+                { con: '클리닉', link: "" },
+                { con: '아산생명과학연구원', link: "" },
+                { con: '기타', link: "" },
+            ]
     },
     {
         id: 2,
         menu: '진료예약/안내',
         link: '',
-        smenu: {
-            stitle: '진료예약 / 안내',
-            scontent: [
-                '병원안내',
-                '외래진료안내',
-                '응급진료안내',
-                '입원진료안내',
-                '가정간호안내',
-                '검사실안내',
-            ],
-        },
+        smenu:
+            [
+                { con: '병원안내', link: "" },
+                { con: '외래진료안내', link: "" },
+                { con: '응급진료안내', link: "" },
+                { con: '입원진료안내', link: "" },
+                { con: '가정간호안내', link: "" },
+                { con: '검사실안내', link: "" },
+            ]
     },
     {
         id: 3,
         menu: '건강정보',
         link: '',
-        smenu: {
-            stitle: '건강정보',
-            scontent: ['의료정보', '건강TV', '헬스에디터'],
-        },
+        smenu:
+            [
+                { con: '의료정보', link: "" },
+                { con: '건강TV', link: "" },
+                { con: '헬스에디터', link: "" },
+            ]
     },
     {
         id: 4,
         menu: '고객서비스',
         link: '',
-        smenu: {
-            stitle: '고객서비스',
-            scontent: ['발급안내', '고객서비스', '이용문의'],
-        },
+        smenu:
+            [
+                { con: '발급안내', link: "" },
+                { con: '고객서비스', link: "" },
+                { con: '이용문의', link: "" },
+            ]
     },
     {
         id: 5,
         menu: '의학교육',
         link: '',
-        smenu: {
-            stitle: '의학교육',
-            scontent: ['학술행사일정', '학술영상다시보기'],
-        },
+        smenu:
+            [
+                { con: '학술행사일정', link: "" },
+                { con: '학술영상다시보기', link: "" },
+            ]
     },
     {
         id: 6,
         menu: '병원이야기',
         link: '',
-        smenu: {
-            stitle: '병원이야기',
-            scontent: [
-                '리얼스토리',
-                'AMC IN(人) SIGHT',
-                '사회공헌',
-                '후원',
-                '자원봉사',
+        smenu:
+            [
+                { con: "리얼스토리", link: "" },
+                { con: "AMC IN(人) SIGHT", link: "" },
+                { con: "사회공헌", link: "" },
+                { con: "후원", link: "" },
+                { con: "자원봉사", link: "" },
             ],
-        },
     },
     {
         id: 7,
         menu: '병원소개',
         link: '',
-        smenu: {
-            stitle: '병원소개',
-            scontent: [
-                '재단소개',
-                '병원소개',
-                '소식',
-                '정기간행물',
-                '교육장안내',
+        smenu:
+            [
+                { con: "재단소개", link: "" },
+                { con: "병원소개", link: "" },
+                { con: "소식", link: "" },
+                { con: "정기간행물", link: "" },
+                { con: "교육장안내", link: "" },
             ],
-        },
     },
 ];
 
 const Header = () => {
+
     return (
         <header className='Header'>
             <div className='top_menu'>
@@ -129,10 +127,9 @@ const Header = () => {
                         return (
                             <li>
                                 <a href={it.link}>{it.menu}</a>
-                                <ul class='smenu'>
+                                <ul class='sub_menu'>
                                     <li>
-                                        <h3>{it.smenu.stitle}</h3>
-                                        <a href=''>{it.smenu.scontent}</a>
+                                        <a href={it.smenu.link}>{it.smenu.con}</a>
                                     </li>
                                 </ul>
                             </li>
