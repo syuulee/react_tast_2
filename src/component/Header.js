2975543e3c637bc4bb57a98e0cf87704b7ae4865
Lee SeoYu <yuyu00lee@gmail.com>
2022-11-02 13:58:31 +0900

    2022.11.02

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,95 +5,87 @@ const GNB = [
         id: 1,
         menu: '의료진/진료과',
         link: '',
-        smenu:
-            [
-                { con: '의료진', link: "" },
-                { con: '진료과', link: "" },
-                { con: '암병원', link: "" },
-                { con: '어린이병원', link: "" },
-                { con: '심장병원', link: "" },
-                { con: '센터', link: "" },
-                { con: '클리닉', link: "" },
-                { con: '아산생명과학연구원', link: "" },
-                { con: '기타', link: "" },
-            ]
+        smenu: [
+            { con: '의료진', link: '' },
+            { con: '진료과', link: '' },
+            { con: '암병원', link: '' },
+            { con: '어린이병원', link: '' },
+            { con: '심장병원', link: '' },
+            { con: '센터', link: '' },
+            { con: '클리닉', link: '' },
+            { con: '아산생명과학연구원', link: '' },
+            { con: '기타', link: '' },
+        ],
     },
     {
         id: 2,
         menu: '진료예약/안내',
         link: '',
-        smenu:
-            [
-                { con: '병원안내', link: "" },
-                { con: '외래진료안내', link: "" },
-                { con: '응급진료안내', link: "" },
-                { con: '입원진료안내', link: "" },
-                { con: '가정간호안내', link: "" },
-                { con: '검사실안내', link: "" },
-            ]
+        smenu: [
+            { con: '병원안내', link: '' },
+            { con: '외래진료안내', link: '' },
+            { con: '응급진료안내', link: '' },
+            { con: '입원진료안내', link: '' },
+            { con: '가정간호안내', link: '' },
+            { con: '검사실안내', link: '' },
+        ],
     },
     {
         id: 3,
         menu: '건강정보',
         link: '',
-        smenu:
-            [
-                { con: '의료정보', link: "" },
-                { con: '건강TV', link: "" },
-                { con: '헬스에디터', link: "" },
-            ]
+        smenu: [
+            { con: '의료정보', link: '' },
+            { con: '건강TV', link: '' },
+            { con: '헬스에디터', link: '' },
+        ],
     },
     {
         id: 4,
         menu: '고객서비스',
         link: '',
-        smenu:
-            [
-                { con: '발급안내', link: "" },
-                { con: '고객서비스', link: "" },
-                { con: '이용문의', link: "" },
-            ]
+        smenu: [
+            { con: '발급안내', link: '' },
+            { con: '고객서비스', link: '' },
+            { con: '이용문의', link: '' },
+        ],
     },
     {
         id: 5,
         menu: '의학교육',
         link: '',
-        smenu:
-            [
-                { con: '학술행사일정', link: "" },
-                { con: '학술영상다시보기', link: "" },
-            ]
+        smenu: [
+            { con: '학술행사일정', link: '' },
+            { con: '학술영상다시보기', link: '' },
+        ],
     },
     {
         id: 6,
         menu: '병원이야기',
         link: '',
-        smenu:
-            [
-                { con: "리얼스토리", link: "" },
-                { con: "AMC IN(人) SIGHT", link: "" },
-                { con: "사회공헌", link: "" },
-                { con: "후원", link: "" },
-                { con: "자원봉사", link: "" },
-            ],
+        smenu: [
+            { con: '리얼스토리', link: '' },
+            { con: 'AMC IN(人) SIGHT', link: '' },
+            { con: '사회공헌', link: '' },
+            { con: '후원', link: '' },
+            { con: '자원봉사', link: '' },
+        ],
     },
     {
         id: 7,
         menu: '병원소개',
         link: '',
-        smenu:
-            [
-                { con: "재단소개", link: "" },
-                { con: "병원소개", link: "" },
-                { con: "소식", link: "" },
-                { con: "정기간행물", link: "" },
-                { con: "교육장안내", link: "" },
-            ],
+        smenu: [
+            { con: '재단소개', link: '' },
+            { con: '병원소개', link: '' },
+            { con: '소식', link: '' },
+            { con: '정기간행물', link: '' },
+            { con: '교육장안내', link: '' },
+        ],
     },
 ];
 
 const Header = () => {
-
     return (
         <header className='Header'>
             <div className='top_menu'>
@@ -128,9 +120,13 @@ const Header = () => {
                             <li>
                                 <a href={it.link}>{it.menu}</a>
                                 <ul class='sub_menu'>
-                                    <li>
-                                        <a href={it.smenu.link}>{it.smenu.con}</a>
-                                    </li>
+                                    {it.smenu.map((its, idx) => {
+                                        return (
+                                            <li>
+                                                <a href={its.link}>{its.con}</a>
+                                            </li>
+                                        );
+                                    })}
                                 </ul>
                             </li>
                         );
